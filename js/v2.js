a888fd394beeade58242a39a89165a20d2fa1e6a
fireorc <fireorc@users.noreply.github.com>
2019-01-08 23:43:57 -0500

    Add files via upload

--- js/v2.js
+++ js/v2.js
@@ -1,1 +1,2 @@
-document.getElementById('footer-content').innerHTML = "© Genuine Domains, an E-Folio Assets subsidiary"
+document.getElementById('footer-content').innerHTML = "© Genuine Domains, an E-Folio Assets subsidiary";
+document.getElementById('cont2').innerHTML = "names (at) genuine.domains";
