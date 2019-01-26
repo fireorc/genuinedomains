d53d987b343c412cec6c73bc3c1843f6a37bbc7d
fireorc <fireorc@users.noreply.github.com>
2019-01-26 14:48:48 -0500

    Add files via upload

--- js/v2.js
+++ js/v2.js
@@ -1,2 +1,10 @@
-document.getElementById('footer-content').innerHTML = "© Genuine Domains, an E-Folio Assets subsidiary";
+document.getElementById('footer-content').innerHTML = "© Genuine Domains, an E-Folio Assets concoction.";
 document.getElementById('cont2').innerHTML = "names (at) genuine.domains";
+function contact() {
+  var x = document.getElementById("contact");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+};
