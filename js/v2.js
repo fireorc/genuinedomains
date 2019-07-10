7af734e920b5923b9ca352408597447022488ae5
fireorc <fireorc@users.noreply.github.com>
2019-07-09 22:21:25 -0400

    Add files via upload

--- js/v2.js
+++ js/v2.js
@@ -1,4 +1,4 @@
-document.getElementById('footer-content').innerHTML = "© Genuine Domains, 2016 to present day";
+document.getElementById('footer-content').innerHTML = "© Genuine Domains, 2016 to present. An E-Folio Assets affiliate.";
 /* in conjunction with css contact and form-deco id
 function contact() {
   var x = document.getElementById("contact");
