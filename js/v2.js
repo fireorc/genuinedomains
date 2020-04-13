3d81d818047924e053eda69374b796b69438cacb
fireorc <fireorc@users.noreply.github.com>
2020-04-13 08:15:53 -0400

    Add files via upload

--- js/v2.js
+++ js/v2.js
@@ -1,4 +1,4 @@
-document.getElementById('footer-content').innerHTML = "<span style='font-size: 18px;'>© Genuine Domains, 2016 to present</span>";
+document.getElementById('footer-content').innerHTML = "<span style='font-size: 18px;'>© Genuine Domains, est. 2016</span>";
 /* in conjunction with css contact and form-deco id
 function contact() {
   var x = document.getElementById("contact");
