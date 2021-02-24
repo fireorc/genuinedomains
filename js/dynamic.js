00a62a0f7f2470d5bb9b013a98fd3b417a91c31b
fireorc <fireorc@users.noreply.github.com>
2021-02-24 12:04:26 -0500

    Add files via upload

--- js/dynamic.js
+++ js/dynamic.js
@@ -114,6 +114,14 @@
 			$('#defi').show();
 		} 
 		// Check if the URL parameter matches
+		else if (dynamicContent == 'gravforce') {
+			$('#gravforce').show();
+		} 
+		// Check if the URL parameter matches
+		else if (dynamicContent == 'defineri') {
+			$('#defineri').show();
+		} 
+		// Check if the URL parameter matches
 		else if (dynamicContent == 'surfer') {
 			$('#surfer').show();
 		} 
